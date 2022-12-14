606a1a43fafce4313b2804a84aa04dcab4e750e2
Derrick Hammer <derrick@derrickhammer.com>
2022-12-14 13:14:24 -0500

    *Update dist

--- dist/index.js
+++ dist/index.js
@@ -9,9 +9,9 @@ export default function preset() {
         plugins: [
             typescript(),
             json(),
+            bundleNativeModulesPlugin(),
             nodeResolve(),
             commonjs(),
-            bundleNativeModulesPlugin(),
         ],
     });
 }
