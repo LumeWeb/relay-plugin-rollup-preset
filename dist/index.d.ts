5e8ee4dab83190d1d556871403d2a55c7f75e7cf
Derrick Hammer <derrick@derrickhammer.com>
2022-12-14 09:54:58 -0500

    *Add dist

---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+declare const preset: import("rollup").RollupOptions;
+export default preset;
---
+++ dist/index.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "rollup";
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { bundleNativeModulesPlugin } from "@lumeweb/rollup-plugin-bundle-native-modules";
+const preset = defineConfig({
+    plugins: [nodeResolve(), commonjs(), bundleNativeModulesPlugin()],
+});
+export default preset;
