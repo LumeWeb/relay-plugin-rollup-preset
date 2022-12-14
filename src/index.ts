773f1b7b756be236529861b3375b174622b3ed7a
Derrick Hammer <derrick@derrickhammer.com>
2022-12-14 10:15:51 -0500

    *Export function that returns a defineConfig

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from "rollup";
+import { defineConfig, RollupOptions } from "rollup";
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { bundleNativeModulesPlugin } from "@lumeweb/rollup-plugin-bundle-native-modules";
 import typescript from "@rollup/plugin-typescript";
 import json from "@rollup/plugin-json";
 
-const preset = defineConfig({
-  plugins: [
-    typescript(),
-    json(),
-    nodeResolve(),
-    commonjs(),
-    bundleNativeModulesPlugin(),
-  ],
-});
-
-export default preset;
+export default function preset(): RollupOptions {
+  return defineConfig({
+    plugins: [
+      typescript(),
+      json(),
+      nodeResolve(),
+      commonjs(),
+      bundleNativeModulesPlugin(),
+    ],
+  });
+}
