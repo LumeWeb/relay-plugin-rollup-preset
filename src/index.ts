546041ad8a1ae76125f16ae67a1c5c4aa110bfc3
Derrick Hammer <derrick@derrickhammer.com>
2022-12-14 10:03:11 -0500

    *Add typescript and json plugins to preset

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,17 @@ import { defineConfig } from "rollup";
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { bundleNativeModulesPlugin } from "@lumeweb/rollup-plugin-bundle-native-modules";
+import typescript from "@rollup/plugin-typescript";
+import json from "@rollup/plugin-json";
 
 const preset = defineConfig({
-  plugins: [nodeResolve(), commonjs(), bundleNativeModulesPlugin()],
+  plugins: [
+    typescript(),
+    json(),
+    nodeResolve(),
+    commonjs(),
+    bundleNativeModulesPlugin(),
+  ],
 });
 
 export default preset;
