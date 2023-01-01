7776703d3f1f655c0252fed7b9046a8bc62c7828
Derrick Hammer <derrick@derrickhammer.com>
2023-01-01 12:40:15 -0500

    *Refactor preset to take the global arguments and merge itself, plus allow node resolve and commonjs overrides

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,34 @@
 import { defineConfig, RollupOptions } from "rollup";
 import commonjs from "@rollup/plugin-commonjs";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
+import {
+  nodeResolve,
+  RollupNodeResolveOptions,
+} from "@rollup/plugin-node-resolve";
 import { bundleNativeModulesPlugin } from "@lumeweb/rollup-plugin-bundle-native-modules";
 import typescript from "@rollup/plugin-typescript";
 import json from "@rollup/plugin-json";
+// @ts-ignore
+import merger from "object-merger";
 
-export default function preset(): RollupOptions {
-  return defineConfig({
-    plugins: [
-      typescript(),
-      json(),
-      bundleNativeModulesPlugin(),
-      nodeResolve({
-        mainFields: ["main"],
-        exportConditions: ["node", "require", "default"],
-      }),
-      commonjs(),
-    ],
-  });
+export default function preset(
+  globalOptions: RollupOptions,
+  resolveOptions: RollupNodeResolveOptions = {},
+  commonJsOptions: any = {}
+): RollupOptions {
+  return merger(
+    globalOptions,
+    defineConfig({
+      plugins: [
+        typescript(),
+        json(),
+        bundleNativeModulesPlugin(),
+        nodeResolve({
+          mainFields: ["main"],
+          exportConditions: ["node", "require", "default"],
+          ...resolveOptions,
+        }),
+        commonjs(commonJsOptions),
+      ],
+    })
+  );
 }
