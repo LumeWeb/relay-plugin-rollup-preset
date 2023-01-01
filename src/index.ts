bdd799928351c75c0509a1e29c204670a93e11db
Derrick Hammer <derrick@derrickhammer.com>
2023-01-01 13:02:27 -0500

    *Remove mainFields

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,6 @@ export default function preset(
         json(),
         bundleNativeModulesPlugin(),
         nodeResolve({
-          mainFields: ["main"],
           ...resolveOptions,
         }),
         commonjs(commonJsOptions),
